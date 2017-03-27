be9ad6ce95467d1995e79103d30d716572a730dc
Matthew Sawatzky <m.sawatty@gmail.com>
2017-03-27 18:37:26 +0000

    Added carrierwave for image uploading

--- Gemfile
+++ Gemfile
@@ -49,6 +49,9 @@ gem 'slim-rails'
 # Add Fancy Box
 gem 'fancybox-rails'
 
+# Add Carrierwave
+gem 'carrierwave', '~> 1.0'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170325011902) do
+ActiveRecord::Schema.define(version: 20170327182651) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -52,11 +52,11 @@ ActiveRecord::Schema.define(version: 20170325011902) do
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.string   "image_url"
     t.decimal  "price"
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
