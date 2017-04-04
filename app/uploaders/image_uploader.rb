d1316decf556c001f7e561b29a3e46bcd00774a5
Matthew Sawatzky <m.sawatty@gmail.com>
2017-04-04 04:10:34 +0000

    Fixing code to rubocop standards

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,4 @@
 class ImageUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -17,8 +16,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name,
+  # "default.png"].compact.join('_'))
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
@@ -37,7 +36,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(jpg jpeg gif png)
+    %w([jpg] [jpeg] [gif] [png])
   end
 
   # Override the filename of the uploaded files:
@@ -45,5 +44,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-
 end
