39db4339330e5380583d0e91c60439afa9baa27b
Matthew Sawatzky <m.sawatty@gmail.com>
2017-04-06 15:00:11 +0000

    Modified for cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :initialize_session
   def index
     @products = Product.all.order(price: :desc).page(params[:page]).per(5)
     @categories = Category.all
@@ -8,20 +9,34 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def cart; end
+
   def filter
     # Get all products
-    @products = Product.all
+    products = Product.all
     # Get category_id and search text from filter form
     @category_id = params[:category_id].to_i
     @search_text = params[:search_text]
     # If category is chosen filter by category_id
-    @products = @products.where(category_id: @category_id) unless @category_id.zero?
+    products = products.where(category_id: @category_id) unless @category_id.zero?
     # If text was entered filter the name by text
-    @products = @products.where('name LIKE ?', "%#{@search_text}%") unless @search_text.empty?
+    products = products.where('name LIKE ?', "%#{@search_text}%") unless @search_text.empty?
     # Get all categories for dropdown
     @categories = Category.all
     # Set pagination for products
-    @products = @products.order(price: :desc).page(params[:page]).per(5)
+    @products = products.order(price: :desc).page(params[:page]).per(5)
     render 'index'
+  end
+
+  def add_to_cart
+    id = params[:id].to_i
+    session[:cart][id] = 1 unless session[:cart][id]
+    render 'cart'
+  end
+
+  private
+
+  def initialize_session
+    session[:cart] ||= {}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'pages#about'
   get 'contact', to: 'pages#contact'
-
   get 'products/filter', to: 'products#filter'
+  get 'cart', to: 'products#cart'
+  post 'add-to-cart', to: 'products#add_to_cart'
 
   resources 'products', only: [:index, :show]
-
-  root to: 'pages#about'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
