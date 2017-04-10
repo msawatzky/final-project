4d7236f381b8878edf8761c68107137328b46ae8
Matthew Sawatzky <m.sawatty@gmail.com>
2017-04-10 23:16:59 +0000

    Moved filter method into Product class

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,19 +12,14 @@ class ProductsController < ApplicationController
   def cart; end
 
   def filter
-    # Get all products
-    products = Product.all
-    # Get category_id and search text from filter form
     @category_id = params[:category_id].to_i
     @search_text = params[:search_text]
-    # If category is chosen filter by category_id
-    products = products.where(category_id: @category_id) unless @category_id.zero?
-    # If text was entered filter the name by text
-    products = products.where('name LIKE ?', "%#{@search_text}%") unless @search_text.empty?
+
+    # Get all products
+    @products = Product.filter_by(@category_id, @search_text)
+
     # Get all categories for dropdown
     @categories = Category.all
-    # Set pagination for products
-    @products = products.order(price: :desc).page(params[:page]).per(5)
     render 'index'
   end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,14 @@ class Product < ApplicationRecord
   validates :name, :description, :price, :category_id, presence: true
   belongs_to :category
   mount_uploader :image, ImageUploader
+
+  def self.filter_by(category_id, search_text)
+    products = Product.all
+    # If category is chosen filter by category_id
+    products = products.where(category_id: category_id) unless category_id.zero?
+    # If text was entered filter the name by text
+    products = products.where('name LIKE ?', "%#{search_text}%") unless search_text.empty?
+    # Set pagination for products
+    products.order(price: :desc).page(params[:page]).per(5)
+  end
 end
